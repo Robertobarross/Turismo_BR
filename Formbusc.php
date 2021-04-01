33b0f532afcef9c7d6eae44ab615e664906d34a0
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-04-01 12:49:45 -0300

    Formulário com barra de busca

---
+++ Formbusc.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Turismo-BR</title>
+</head>
+<body>
+
+
+
+<div id="search" align="center"><!-- Inicio da div search -->
+    <form name="busc" method="POST" action="busc.php"><!-- Inicio do formulário busc -->
+    <input type="search" name="buscar" class="busc" placeholder="Pesquisar lugares" ><img src="Imagens/search.png" class="imglupa"><!-- Barra de pesquisa e imagem da lupa -->
+    </form><!-- Fim do formulário busc -->
+
+    <style type="text/css">/* Inicio da estilização do formulário busc */
+    .busc{width: 60%; height: 40px; border-radius: 10px; margin-top: 15px; font-size: 20px; font-family: Arial;}/* Estilo da barra de pesquisa */
+    .imglupa{width: 20px; margin-left: -30px; margin-top: 5px;}/* Estilo da lupa */
+    </style><!-- Fim da estilização do formulário busc -->
+</div><!-- Fim da div seach -->
+
+
+    
+</body>
+</html>
