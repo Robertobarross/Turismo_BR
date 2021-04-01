3df4d02211b15e032498a94878074a51a797e90a
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-04-01 12:49:17 -0300

    Conexão com banco de dados

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+
+// Arquivo de conexão com o banco de dados db_jornal_ti //
+// Variáveis da conxão //
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "Turismo-BR";
+
+// link da conexão //
+$link = mysqli_connect($host, $user, $pass, $db);
+
+?>
