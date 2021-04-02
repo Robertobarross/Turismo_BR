60c8a9a84370fb7ae9b726bddbd869e02c17f08a
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-04-01 21:51:21 -0300

    Arquivo de busca
    Conteúdo responsável pela postagem depois que é feita a pesquisa na página inicial.

--- Busc.php
+++ Busc.php
@@ -12,37 +12,65 @@ include "connect.php"; // Arquivo para conexão com o banco de dados //
 </head>
 <body>
 
+
 <!-- Inicio da div bucar -->
-<div id="busc" align="center">
+<div id="buscar" align="center">
     <style type="text/css">/* Configuração dos intens da div */
-    body {background-image: url("imagens/img2.jpg");} /* Imagem cor de fundo */
+    body {background-image: url("Imagens/img2.jpg");} /* Imagem cor de fundo */
     </style>
 
 <?php // Arquivo de busca no banco de dados e variavéis da conexão //
-    $busc=$_POST['busc'];
+    @$busc = $_POST['busc'];
     $sql = mysqli_query($link, "SELECT * FROM cadviagem WHERE cidade LIKE '%".$busc."%'");
     $row = mysqli_num_rows($sql);
     if($row > 0) {
-    while($linha = mysqli_fetch_array($sql)) {
-    @$foto1 = $linha['foto1'];
-    $valor = $linha['valor'];
-    $produto = $linha['produto'];
-
-    echo "<br/><br/>";
-    echo "<strong>Produto: </strong>".$produto;
-    echo "<br/><br/>";
-    echo "<strong>Valor: </strong>".$valor;
-    echo "<br/><br/>";       
-    }
+    while($linha = mysqli_fetch_array($sql)) { // Iniciando as variáveis //
+    $cidade = $linha['cidade'];
+    $foto1 = $linha['foto1'];
+    $descricao1 = $linha['descricao1'];
+    $lugar2 = $linha['lugar2'];
+    $foto2 = $linha['foto2'];
+    $descricao2 = $linha['descricao2'];
+    } // Finalizando as variavéis //
     }
 ?>
-    <!-- Carrega a imagem da pasta -->
-    <br><img src="Imagens/<?php echo "post"."/".@$foto1;?>"class="#"> 
+
+    <!-- Iniciando arquivo da postagem superior -->
+    <a class="texto <?php echo "cidade>" .$cidade;?>;">
+<?php
+    echo "</br></br>";
+    echo "<strong>Cidade: </strong>".$cidade;
+    echo "<br/><br/>";  
+    echo "<strong>Atrativos: </strong>" .$descricao1;
+    echo "<br/></br>";
+?>
+    <br><img src="Postagens/<?php echo ""."/".$foto1;?>"class="imagem">
+    <!-- Finalizando arquivo da postagem superior -->
+ 
+
+    <!-- Iniciando arquivo da postagem inferior -->
+    <a class="texto <?php echo "lugar2>" .$lugar2;?>"
+<?php
+    echo "</br></br>";
+    echo "<strong>Lugar: </strong>" .$lugar2; 
+    echo "<br/></br>";
+    echo "<strong>Atrativos: </strong>" .$descricao2;
+    echo "<br/></br>";
+?>
+    <br><img src="Postagens/<?php echo ""."/".$foto2;?>"class="imagem" align="left"></br> 
+    <!-- Finalizando arquivo da postagem inferior -->
+
+
     <!-- Botões de navegação -->
     <p><input name="entrar" type="button" id="button4" onclick=location.href='#' value="#" class="bt"/>
     <p><input name="entrar" type="button" id="button4" onclick=location.href='#' value="#" class="bt"/>
     <!-- Link para retornar para página inicial -->
     <p><a href="Index.php" class="link">Voltar para página inicial</a></p>
+
+    <style type="text/css">
+    .texto{font-size: 30px; color: white; text-align: justify;}
+    .imagem{width: 50%;}
+    </style>
 
 </div>
 <!-- Fim da div bucar -->
